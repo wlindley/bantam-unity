876533083dee0e46714359d87fa20710c06a953a
Walker Lindley <walker@walkerlindley.com>
2017-11-18 15:20:26 -0800

    Implement Context class

---
+++ Assets/Bantam/Scripts/Runtime/Context.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+
+namespace Bantam.Unity
+{
+	public abstract class Context
+	{
+		private readonly List<Factory> factories = new List<Factory>();
+
+		public abstract void Init();
+
+		public virtual T GetInstance<T>(string id="") where T : class
+		{
+			var type = typeof(T);
+			var numFactories = factories.Count;
+			for (var i = 0; i < numFactories; i++)
+			{
+				var instance = factories[i].Build(type, id);
+				if (null != instance)
+					return instance as T;
+			}
+			return null;
+		}
+
+		public virtual void Inject<T>(out T injectee, string id="") where T : class
+		{
+			injectee = GetInstance<T>(id);
+		}
+
+		public virtual void BindSingleton<T>(string id="") where T : class, new()
+		{
+			factories.Add(new SingletonFactory<T>(id));
+		}
+
+		public virtual void BindTransient<T>(string id="") where T : class, new()
+		{
+			factories.Add(new TransientFactory<T>(id));
+		}
+
+		public virtual void BindInstance<T>(T instance, string id="") where T : class
+		{
+			factories.Add(new InstanceFactory<T>(instance, id));
+		}
+
+		public virtual void BindCustom(Factory factory)
+		{
+			factories.Add(factory);
+		}
+	}
+}
+
---
+++ Assets/Bantam/Scripts/Runtime/Factory.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Bantam.Unity
+{
+	public interface Factory
+	{
+		object Build(Type type, string id);
+	}
+
+	internal abstract class BaseFactory : Factory
+	{
+		protected string id;
+
+		internal BaseFactory(string id)
+		{
+			this.id = id;
+		}
+
+		public abstract object Build(Type Type, string id);
+	}
+
+	internal class SingletonFactory<T> : BaseFactory where T : class, new()
+	{
+		private T instance;
+
+		internal SingletonFactory(string id) : base(id) {}
+
+		public override object Build(Type type, string id)
+		{
+			if (typeof(T) != type || this.id != id)
+				return null;
+			if (null == instance)
+				instance = new T();
+			return instance;
+		}
+	}
+
+	internal class TransientFactory<T> : BaseFactory where T : class, new()
+	{
+		internal TransientFactory(string id) : base(id) {}
+
+		public override object Build(Type type, string id)
+		{
+			if (typeof(T) != type || this.id != id)
+				return null;
+			return new T();
+		}
+	}
+
+	internal class InstanceFactory<T> : BaseFactory where T : class
+	{
+		private T instance;
+
+		internal InstanceFactory(T instance, string id) : base(id)
+		{
+			this.instance = instance;
+		}
+
+		public override object Build(Type type, string id)
+		{
+			if (typeof(T) != type || this.id != id)
+				return null;
+			return instance;
+		}
+	}
+}
